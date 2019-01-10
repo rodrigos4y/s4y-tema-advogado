aa8aeb0379444d291722f0ab80f1b45fa1628309
Rodrigo Dias de Freitas <rodrigodias@e-corp.acerp.br>
2019-01-10 17:04:21 -0200

    Colocar o menu dinâmico
    - Criado o menu do cabeçalho
    - Criado o menu do rodapé

--- functions.php
+++ functions.php
@@ -3,6 +3,7 @@
     add_action('wp_footer', 's4l_add_script_rodape');
     add_theme_support('post-thumbnails');
     add_image_size('noticias-home', 640, 426, true);
+    add_action('init', 's4l_action_init');
 
     function s4l_add_script_cabecalho(){
 
@@ -22,6 +23,11 @@
         wp_enqueue_script('easing-s4l', get_stylesheet_directory_uri() . '/js/easing.js');
         wp_enqueue_script('move-top-s4l', get_stylesheet_directory_uri() . '/js/move-top.js');
         wp_enqueue_script('bootstrap-s4l', get_stylesheet_directory_uri() . '/js/bootstrap.js');
+    }
+
+    function s4l_action_init(){
+        register_nav_menu('s4l_principal', 'Menu Principal (cabeçalho)');
+        register_nav_menu('s4l_rodape', 'Menu Rodapé');
     }
 
     //Custom post com custom field
@@ -138,6 +144,11 @@
                 break;
         }
     }
+
+    function add_menuclass($ulclass) {
+        return preg_replace('/<a /', '<a class="scroll"', $ulclass);
+     }
+    add_filter('wp_nav_menu','add_menuclass');
 
     require_once('post-area.php');
 ?>
